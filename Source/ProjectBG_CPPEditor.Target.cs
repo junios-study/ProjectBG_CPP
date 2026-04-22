0bd5cbc18a735610138d965f4bd18612202f4a93
junios-study <junios@naver.com>
2026-04-22 09:32:26 +0900

    기본 클래스 생성

---
+++ Source/ProjectBG_CPPEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ProjectBG_CPPEditorTarget : TargetRules
+{
+	public ProjectBG_CPPEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+
+		ExtraModuleNames.AddRange( new string[] { "ProjectBG_CPP" } );
+	}
+}
