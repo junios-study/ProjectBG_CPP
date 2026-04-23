895fd1c7ca4d3caf159f9d179966cc9a53bb0072
junios-study <junios@naver.com>
2026-04-23 13:52:37 +0900

    Animation Blueprint, AnimInstance 시작

---
+++ Source/ProjectBG_CPP/Basic/BasicAnimInstance.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BasicAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBG_CPP_API UBasicAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float Speed;
+
+};
